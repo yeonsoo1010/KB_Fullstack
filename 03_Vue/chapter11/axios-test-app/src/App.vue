f0f6342eac59ea15ba408c4ae83360ae4c731aa4
yeonsoo1010 <meriel1010@hanyang.ac.kr>
2024-06-03 18:06:00 +0900

    6.3

---
+++ 03_Vue/chapter11/axios-test-app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from "axios";
+
+const requestAPI = () => {
+  const url = "http://localhost:3000/todos";
+  axios.get(url).then((response) => {
+    console.log("# 응답객체: ", response);
+  });
+};
+
+requestAPI();
+</script>
+
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
